527050477a659f2df93b90289355f414bcb75031
Zohaib haider <zebihaider123@gmail.com>
2023-10-28 18:16:40 +0500

    changes

--- todolist/src/modules/Task.js
+++ todolist/src/modules/Task.js
@@ -24,8 +24,8 @@ const Task = (props) => {
         <div className='flex flex-row gap-x-2 w-full justify-between p-2 rounded-lg shadow-sm shadow-black' key={props.index}>
             <p className={`text-white text-xl font-custom cursor-pointer ${isChecked ? 'line-through' : ''}`}>{props.credentials}</p>
             <div className='flex flex-row gap-x-4 justify-center items-center '>
-                <FontAwesomeIcon icon={isChecked ? faXmark : faCheck} className='text-white font-custom font-bold text-lg cursor-pointer' onClick={check} />
-                <FontAwesomeIcon icon={faTrash} className='text-white font-custom text-lg cursor-pointer ' onClick={() => props.delteTask(props.index)} />
+                <FontAwesomeIcon icon={isChecked ? faXmark : faCheck} className='text-white font-custom font-bold text-lg cursor-pointer hover:shadow-sm hover:shadow-black transition-all duration-300 ease-in-out p-2 rounded-full' onClick={check} />
+                <FontAwesomeIcon icon={faTrash} className='text-white font-custom text-lg cursor-pointer hover:shadow-sm hover:shadow-black transition-all duration-300 ease-in-out p-2 rounded-full' onClick={() => props.delteTask(props.index)} />
             </div>
         </div>
     )
